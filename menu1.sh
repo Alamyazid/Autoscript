a57f96465433d45322c48671b446f8510fb962fa
Tuan BabaL <48976654+Alamyazid@users.noreply.github.com>
2021-07-27 02:06:32 +0800

    Add files via upload

---
+++ menu1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#Menu
+clear
+echo -e "* menu              : Menampilkan daftar perintah"
+echo -e "* usernew         : Membuat akun SSH & OpenVPN"
+echo -e "* trial                 : Membuat akun trial"
+echo -e "* hapus             : Menghapus akun SSH & OpenVPN"
+echo -e "* cek                 : Cek user login"
+echo -e "* cekconfig      : Cek config yang tersedia"
+echo -e "* member         : Daftar member SSH & OpenVPN"
+echo -e "* restart            : Restart service Openssh,Dropbear"
+echo -e "                    Stunnel,OpenVPN dan Shadowsocks"
+echo -e "* reboot            : Reboot VPS"
+echo -e "* speedtest      : Speedtest VPS"
+echo -e "* info            : Menampilkan informasi sistem"
+echo -e "* about           : Info script auto install"
+echo -e "* exit            : Keluar dari Putty/Connecbot/"
+echo -e "* addhost       : Menambahkan host"
+echo -e ""
