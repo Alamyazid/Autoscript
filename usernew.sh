84a3101af8ec6803f22d0f2ed51ea57df430d767
Tuan BabaL <48976654+Alamyazid@users.noreply.github.com>
2021-07-27 14:17:31 +0800

    Add files via upload

---
+++ usernew.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+read -p "Username : " Login
+read -p "Password : " Pass
+read -p "Expired (hari): " masaaktif
+
+IP=$(wget -qO- icanhazip.com);
+domain=$(cat /etc/v2ray/domain);
+echo Script AutoCreate Akun SSH dan OpenVPN Mod By TuanYz
+sleep 1
+echo Ping Host
+echo Cek Hak Akses...
+sleep 0.5
+echo Permission Accepted
+clear
+sleep 0.5
+echo Membuat Akun: $Login
+sleep 0.5
+echo Setting Password: $Pass
+sleep 0.5
+clear
+useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $Login
+exp="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
+echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
+echo -e ""
+echo -e "Terima Kasih Telah Menggunakan Layanan Kami"
+echo -e "Informasi Akun SSH & OpenVPN"
+echo -e "Username          : $Login "
+echo -e "Password          : $Pass"
+echo -e "==============================="
+echo -e "IP Server         : $IP"
+echo -e "Host              : $domain"
+echo -e "OpenSSH           : 22, 500"
+echo -e "Dropbear          : 143, 109"
+echo -e "WS-SSH            : 80 ,8880"
+echo -e "WS-SSH SSL/TLS    : 443"
+echo -e "SSL/TLS           : 444"
+echo -e "BadVPN/UDPGW      : 7100, 7200, 7300"
+echo -e "Squid             : 3128, 8080 (limit to IP SSH)" 
+echo -e "OpenVPN           : TCP 1194 http://$IP:81/client-tcp-1194.ovpn"
+echo -e "OpenVPN           : UDP 2200 http://$IP:81/client-udp-2200.ovpn"
+echo -e "OpenVPN           : SSL 992 http://$IP:81/client-tcp-ssl.ovpn"
+echo -e "==============================="
+echo -e "Aktif Sampai      : $exp"
+echo -e "==============================="
+echo -e "Payload Default WS:"
+echo -e "GET / HTTP/1.1[crlf]Host: $domain[crlf]Connection: Keep-Alive[crlf]User-Agent: [ua][crlf]Upgrade: websocket[crlf][crlf]"
+echo -e "Script Mod By TuanYz"
